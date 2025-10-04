57359d61ed7724aa19c031bef28098d2ad2cd948
kwa11 <khalid.abojmei@app4legal.com>
2025-10-04 03:36:11 +0300

    modification

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,18 @@
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  document.title = 'Pate Cream - Menu'
+</script>
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap');
 
 #app {
-  background-color: #FFF1FB;
+  background-color: #fff1fb;
   min-height: 100vh;
   font-family: 'Montserrat', sans-serif;
 }
